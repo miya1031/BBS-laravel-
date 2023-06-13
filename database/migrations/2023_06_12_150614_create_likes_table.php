565b012217469a146e3ec85864214acd67455952
miyake <yujiro.miyake@rightcode.co.jp>
2023-06-13 17:06:45 +0900

    migrationの修正

--- database/migrations/2023_06_12_150614_create_likes_table.php
+++ database/migrations/2023_06_12_150614_create_likes_table.php
@@ -1,20 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('likes', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table): void {
             $table->id();
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
-            $table->foreignId('member_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->datetime('created_at');
             $table->datetime('updated_at');
         });
