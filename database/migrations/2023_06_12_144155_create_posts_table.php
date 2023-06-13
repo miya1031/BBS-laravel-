0cfac02a1119da354c483f8f663124ba39b1295d
miyake <yujiro.miyake@rightcode.co.jp>
2023-06-13 19:10:02 +0900

    Revert "Revert "migrationの修正""
    This reverts commit 2b688ec5b6db197f39b0a4c018fc12c098004702.

--- database/migrations/2023_06_12_144155_create_posts_table.php
+++ database/migrations/2023_06_12_144155_create_posts_table.php
@@ -1,21 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table): void {
             $table->id();
-            $table->foreignId('member_id')->constrained()->onDelete('cascade');//外部キー制約/投稿者が退会した場合は投稿も削除
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); //外部キー制約/投稿者が退会した場合は投稿も削除
             $table->text('message');
-            $table->integer('reply_post_id')->nullable();//返信でない場合はNULL
+            $table->integer('reply_post_id')->nullable(); //返信でない場合はNULL
             $table->datetime('created_at');
             $table->datetime('updated_at');
         });
