d048936a079102d096fdf6acbe071b3906a960b5
miyake <yujiro.miyake@rightcode.co.jp>
2023-06-14 10:33:14 +0900

    Revert "Revert "Revert "migrationの修正"""
    This reverts commit 0cfac02a1119da354c483f8f663124ba39b1295d.

--- database/migrations/2023_06_12_134932_create_members_table.php
+++ database/migrations/2023_06_12_134932_create_members_table.php
@@ -1,20 +1,20 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('members', function (Blueprint $table): void {
+        Schema::create('members', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); //外部キー制約/投稿者が退会した場合はアイコンも削除
+            $table->string('name');
+            $table->string('email')->unique();
             $table->string('icon');
             $table->datetime('created_at');
             $table->datetime('updated_at');
@@ -24,7 +24,7 @@ return new class () extends Migration {
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down(): void    
     {
         Schema::dropIfExists('members');
     }
--- database/migrations/2023_06_12_154330_create_retweets_table.php
+++ database/migrations/2023_06_12_154330_create_retweets_table.php
@@ -1,21 +1,20 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('retweets', function (Blueprint $table): void {
+        Schema::create('retweets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('member_id')->constrained()->onDelete('cascade');
             $table->datetime('created_at');
             $table->datetime('updated_at');
         });
