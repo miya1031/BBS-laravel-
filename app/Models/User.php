2f489bcf2738ebb3f4b1747730567ee24db266a2
miyake <yujiro.miyake@rightcode.co.jp>
2023-06-20 18:53:12 +0900

    返り値の型宣言

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-    public function emailExists(string $email)
+    public function emailExists(string $email): bool
     {
         $emailExists = self::query()
             ->where('email', '=', $email)
