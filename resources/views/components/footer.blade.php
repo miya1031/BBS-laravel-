f24e2f058d462b02639c982b0d02d61dc583865e
miya1031 <102223422+miya1031@users.noreply.github.com>
2023-06-29 19:38:06 +0900

    Revert "Feature/posts list"

--- resources/views/components/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class='footer footer-center p-4 bg-primary-content text-base-content'>
-    <div>
-        <p>Copyright © 2023 - All right reserved by YUJIRO MIYAKE</p>
-    </div>
-</footer>
